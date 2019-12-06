7d4e4379913c390f7f9ac6915ea85d847d1b81ab
Abhay Sharma <abhays948@gmail.com>
2019-12-06 22:31:34 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import './App.css';
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
-    }
-  }
+import './ShowSubscribers.css';
+class ShowSubscribers extends Component {
 
   render() {
 
@@ -23,7 +17,7 @@ class App extends Component {
           </div>
 
            {
-        this.state.subscribersListToShow.map(sub => {
+         this.props.subscribersList.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
@@ -38,4 +32,4 @@ class App extends Component {
   );
 }
 }
-export default App;
+export default ShowSubscribers;
